1370b84b35a770ce8511cceac40bb067f2b24df2
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-01 11:03:32 +0300

    connect to db established

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
 const app = express();
+// isitraukiam mongoose
+const mongoose = require('mongoose');
 
 const { mongoDbString } = require('./config/config');
 
 // console.log(' mongoDbString', mongoDbString);
+// prisijungimas prie duomenu bazes
+mongoose.connect(mongoDbString);
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
